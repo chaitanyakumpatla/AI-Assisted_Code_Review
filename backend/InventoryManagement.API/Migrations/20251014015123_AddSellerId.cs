4c57905ebb9cf028227497e454fe8da2e0a539ad
Kumpatla Sai Chaitanya <67018367+chaitanyakumpatla@users.noreply.github.com>
2025-11-23 15:41:20 +0530

    Add files via upload

---
+++ backend/InventoryManagement.API/Migrations/20251014015123_AddSellerId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSellerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SellerId",
+                table: "Inventories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SellerId",
+                table: "Inventories");
+        }
+    }
+}
